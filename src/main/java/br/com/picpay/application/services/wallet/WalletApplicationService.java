d7dc0a5cb0255e44ab33d71ea0bc9a7a98fc7996
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-01-27 02:21:20 -0300

    wallet applicaiton serivce

---
+++ src/main/java/br/com/picpay/application/services/wallet/WalletApplicationService.java
@@ -0,0 +1,36 @@
+package br.com.picpay.application.services.wallet;
+
+import br.com.picpay.application.dtos.wallet.WalletUserResponse;
+import br.com.picpay.domain.entities.wallet.Wallet;
+import br.com.picpay.infra.repositories.wallet.IWalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class WalletApplicationService {
+
+    private final IWalletRepository walletRepository;
+
+    public Wallet create(Wallet walletRequest) {
+        if (walletRepository.existsByBalance(walletRequest.getBalance())) {
+            throw new IllegalArgumentException("Wallet already exists");
+        }
+
+        return this.walletRepository.save(walletRequest);
+    }
+
+    public Map<UUID, WalletUserResponse> getWallets(UUID payerId, UUID receiverId) {
+        WalletUserResponse walletPayer = this.walletRepository.findWalletUserByWalletId(payerId)
+                .orElseThrow(() -> new IllegalArgumentException("Payer not found"));
+
+        WalletUserResponse walletReceiver = this.walletRepository.findWalletUserByWalletId(receiverId)
+                .orElseThrow(() -> new IllegalArgumentException("Receiver not found"));
+
+        return Map.of(walletPayer.walletId(), walletPayer, walletReceiver.walletId(), walletReceiver);
+    }
+
+}
