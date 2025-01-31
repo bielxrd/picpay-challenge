9611f14b9a2e857d504c990af51836b75cfcfd4a
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-01-30 22:04:18 -0300

    enable async

--- src/main/java/br/com/picpay/PicpayChallengeApplication.java
+++ src/main/java/br/com/picpay/PicpayChallengeApplication.java
@@ -2,8 +2,10 @@ package br.com.picpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PicpayChallengeApplication {
 
     public static void main(String[] args) {
