30c2b2f1ad3ce0c1df5edf95fc4923745190bb74
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-01-30 22:04:09 -0300

    ajusta facade para enviar mensagem de email para sqs

--- src/main/java/br/com/picpay/application/facade/TransferFacade.java
+++ src/main/java/br/com/picpay/application/facade/TransferFacade.java
@@ -8,7 +8,9 @@ import br.com.picpay.application.dtos.wallet.WalletUserResponse;
 import br.com.picpay.application.services.balance.BalanceApplicationService;
 import br.com.picpay.application.services.transfer.TransferApplicationService;
 import br.com.picpay.application.services.wallet.WalletApplicationService;
+import br.com.picpay.infra.services.sqs.SqsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +24,10 @@ public class TransferFacade {
     private final WalletApplicationService walletApplicationService;
     private final TransferApplicationService transferApplicationService;
     private final BalanceApplicationService balanceApplicationService;
+    private final SqsService sqsService;
+
+    @Value("${spring.cloud.aws.sqs.endpoint}")
+    private String queueName;
 
     @Transactional(rollbackFor = Exception.class)
     public TransferResponse transfer(TransferRequest transferRequest) {
@@ -29,7 +35,23 @@ public class TransferFacade {
 
         this.balanceApplicationService.transferBalance(wallets.get(transferRequest.getPayerId()), wallets.get(transferRequest.getReceiverId()), transferRequest.getValue());
 
-        return this.transferApplicationService.createTransfer(wallets.get(transferRequest.getPayerId()), wallets.get(transferRequest.getReceiverId()), transferRequest.getValue());
+        var transferResponse = this.transferApplicationService.createTransfer(wallets.get(transferRequest.getPayerId()), wallets.get(transferRequest.getReceiverId()), transferRequest.getValue());
+
+        Map<String, Object> messageAttributesReceiver = Map.of("receiver", transferResponse.getReceiver().getName(),
+                "value", transferRequest.getValue(),
+                "payerName", transferResponse.getPayer().getName(),
+                "transferDate", transferResponse.getTransferDate());
+
+        this.sqsService.sendEmailMessage(queueName, transferResponse.getReceiver().getEmail(), messageAttributesReceiver);
+
+        Map<String, Object> messageAttributesPayer = Map.of("payer", transferResponse.getReceiver().getName(),
+                "value", transferRequest.getValue(),
+                "receiverName", transferResponse.getPayer().getName(),
+                "transferDate", transferResponse.getTransferDate());
+
+        this.sqsService.sendEmailMessage(queueName, transferResponse.getPayer().getEmail(), messageAttributesPayer);
+
+        return transferResponse;
     }
 
     public BaseResponsePageable<List<TransfersListResponse>> getTransfersReceivedByUserIdAndPageable(UUID userId, int pageNumber, int pageSize) {
