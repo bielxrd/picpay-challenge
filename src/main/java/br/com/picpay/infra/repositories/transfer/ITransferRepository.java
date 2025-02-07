c5c53d39bfbb29f2e73053b7e3748d1e7691f8f7
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-07 00:50:00 -0300

    feat: lista de transferencias de um receiver e date between

--- src/main/java/br/com/picpay/infra/repositories/transfer/ITransferRepository.java
+++ src/main/java/br/com/picpay/infra/repositories/transfer/ITransferRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,4 +13,5 @@ public interface ITransferRepository extends JpaRepository<Transfer, UUID> {
     Page<Transfer> findByReceiverId(UUID userId, Pageable pageable);
     Page<Transfer> findByPayerId(UUID userId, Pageable pageable);
     Page<Transfer> findByPayerIdOrReceiverId(UUID payerId, UUID receiverId, Pageable pageable);
+    List<Transfer> findByReceiverIdAndTransferDateBetween(UUID userId, LocalDateTime startDate, LocalDateTime endDate);
 }
