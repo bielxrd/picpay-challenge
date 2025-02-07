195efd2fb7edf9297668f0268f8572e1b208ae5c
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-07 00:48:26 -0300

    feat: utils para buildar EmaiLrequest

---
+++ src/main/java/br/com/picpay/shared/utils/MessageUtils.java
@@ -0,0 +1,63 @@
+package br.com.picpay.shared.utils;
+
+import software.amazon.awssdk.services.ses.model.*;
+
+public class MessageUtils {
+    public static SendEmailRequest buildEmailRequestReceiver(String emailFrom, String emailTo, String receiverName, String payerName, double value, String transferDate) {
+        SendEmailRequest emailRequest = SendEmailRequest.builder()
+                .source(emailFrom)
+                .destination(Destination.builder()
+                        .toAddresses(emailTo)
+                        .build())
+                .message(Message.builder()
+                        .subject(Content.builder()
+                                .data("Transferência recebida")
+                                .charset("UTF-8")
+                                .build())
+                        .body(Body.builder()
+                                .text(Content.builder()
+                                        .data(String.format(
+                                                "Olá %s,\n\nVocê recebeu uma transferência de %s no valor de %s em %s.",
+                                                receiverName,
+                                                payerName,
+                                                value,
+                                                transferDate
+                                        ))
+                                        .charset("UTF-8")
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+
+        return emailRequest;
+    }
+
+    public static SendEmailRequest buildEmailRequestPayer(String emailFrom, String emailTo, String receiverName, String payerName, double value, String transferDate) {
+        SendEmailRequest emailRequest = SendEmailRequest.builder()
+                .source(emailFrom)
+                .destination(Destination.builder()
+                        .toAddresses(emailTo)
+                        .build())
+                .message(Message.builder()
+                        .subject(Content.builder()
+                                .data("Transferência realizada")
+                                .charset("UTF-8")
+                                .build())
+                        .body(Body.builder()
+                                .text(Content.builder()
+                                        .data(String.format(
+                                                "Olá %s,\n\nVocê realizou uma transferência para %s no valor de %s em %s.",
+                                                payerName,
+                                                receiverName,
+                                                value,
+                                                transferDate
+                                        ))
+                                        .charset("UTF-8")
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+
+        return emailRequest;
+    }
+}
