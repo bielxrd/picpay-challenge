33f7ef9f8ea8318819f716129a651e6ea05b5ad4
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-07 00:52:47 -0300

    feat: facade para profile

--- src/main/java/br/com/picpay/application/facade/UserFacade.java
+++ src/main/java/br/com/picpay/application/facade/UserFacade.java
@@ -1,17 +1,22 @@
 package br.com.picpay.application.facade;
 
 import br.com.picpay.application.dtos.auth.AuthResponse;
+import br.com.picpay.application.dtos.user.UserProfileDto;
 import br.com.picpay.application.dtos.user.create.UserRequest;
 import br.com.picpay.application.dtos.user.create.UserResponse;
 import br.com.picpay.application.factory.UserStrategyFactory;
 import br.com.picpay.application.services.auth.AuthApplicationService;
+import br.com.picpay.application.services.user.UserApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserFacade {
     private final AuthApplicationService authApplicationService;
+    private final UserApplicationService userApplicationService;
     private final UserStrategyFactory userStrategyFactory;
 
     public UserResponse createUser(UserRequest userRequest) {
@@ -26,5 +31,9 @@ public class UserFacade {
 
     public AuthResponse auth(String email, String password) {
         return this.authApplicationService.auth(email, password);
+    }
+
+    public UserProfileDto getUserProfile(UUID id) {
+        return this.userApplicationService.getUserProfile(id);
     }
 }
