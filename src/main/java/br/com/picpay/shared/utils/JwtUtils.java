2502ea2275accaa45da1eb0350a5a47f75a64310
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-07 00:48:43 -0300

    feat: muda para static inves de usar DI

--- src/main/java/br/com/picpay/shared/utils/JwtUtils.java
+++ src/main/java/br/com/picpay/shared/utils/JwtUtils.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,8 +19,15 @@ public class JwtUtils {
     @Value("${jwt-secret-key}")
     public String secretKey;
 
-    public String generateToken(UUID userId, ERole role) {
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    private static String staticSecretKey;
+
+    @PostConstruct
+    public void init() {
+        staticSecretKey = secretKey;
+    }
+
+    public static String generateToken(UUID userId, ERole role) {
+        Algorithm algorithm = Algorithm.HMAC256(staticSecretKey);
         Instant expiresIn = Instant.now().plus(Duration.ofHours(1));
        return JWT.create().withIssuer("picpay")
                 .withSubject(userId.toString())
@@ -28,10 +36,10 @@ public class JwtUtils {
                 .sign(algorithm);
     }
 
-    public DecodedJWT decodeToken(String token) {
+    public static DecodedJWT decodeToken(String token) {
         token = token.replace("Bearer ", "");
         try {
-            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            Algorithm algorithm = Algorithm.HMAC256(staticSecretKey);
             return JWT.require(algorithm).build().verify(token);
         } catch (JWTVerificationException e) {
             throw new RuntimeException("Invalid token");
