97f54bf49612523b1c104d60dfee32f344606c5e
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-07 00:51:28 -0300

    feat: rota para profile

--- src/main/java/br/com/picpay/application/controllers/UserController.java
+++ src/main/java/br/com/picpay/application/controllers/UserController.java
@@ -1,14 +1,15 @@
 package br.com.picpay.application.controllers;
 
+import br.com.picpay.application.dtos.user.UserProfileDto;
 import br.com.picpay.application.dtos.user.create.UserRequest;
 import br.com.picpay.application.dtos.user.create.UserResponse;
 import br.com.picpay.application.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -21,6 +22,12 @@ public class UserController {
     public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest) {
         UserResponse user = this.userFacade.createUser(userRequest);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserProfileDto> getUserProfile(Principal principal) {
+        UUID id = UUID.fromString(principal.getName());
+        return ResponseEntity.ok(userFacade.getUserProfile(id));
     }
 
 }
