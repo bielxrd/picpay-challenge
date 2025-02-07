da9edc31ff6921fe8a3d718a6722f6518b15f125
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-07 00:52:20 -0300

    feat: metodo para profile dto

--- src/main/java/br/com/picpay/application/services/user/UserApplicationService.java
+++ src/main/java/br/com/picpay/application/services/user/UserApplicationService.java
@@ -1,11 +1,14 @@
 package br.com.picpay.application.services.user;
 
+import br.com.picpay.application.dtos.user.UserProfileDto;
 import br.com.picpay.application.dtos.user.create.UserRequest;
 import br.com.picpay.domain.entities.user.User;
 import br.com.picpay.domain.services.UserDomainService;
 import br.com.picpay.infra.repositories.user.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,10 @@ public class UserApplicationService {
         }
 
         return this.userRepository.save(userRequest.toEntity());
+    }
+
+    public UserProfileDto getUserProfile(UUID id) {
+        return this.userRepository.findByIdCustom(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
