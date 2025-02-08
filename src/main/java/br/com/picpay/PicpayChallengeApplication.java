a4e3774ba643320fcf87605720371ca3c86a698d
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-08 18:09:01 -0300

    feat: add redis config

--- src/main/java/br/com/picpay/PicpayChallengeApplication.java
+++ src/main/java/br/com/picpay/PicpayChallengeApplication.java
@@ -2,14 +2,17 @@ package br.com.picpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class PicpayChallengeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PicpayChallengeApplication.class, args);
+        System.setProperty("spring.devtools.restart.enabled", "false");
     }
 
 }
