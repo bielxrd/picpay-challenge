930bc0e7b26b0994f144e77c2a8f2dcb3ef38704
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-08 18:10:14 -0300

    fix

--- src/main/java/br/com/picpay/application/dtos/BaseResponsePageable.java
+++ src/main/java/br/com/picpay/application/dtos/BaseResponsePageable.java
@@ -1,16 +1,20 @@
 package br.com.picpay.application.dtos;
 
+import br.com.picpay.application.dtos.transfer.TransfersListResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.List;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class BaseResponsePageable<T>{
-    private T data;
+public class BaseResponsePageable implements Serializable {
+    private List<TransfersListResponse> data;
     private int pageNumber;
     private int pageSize;
     private long totalItems;
