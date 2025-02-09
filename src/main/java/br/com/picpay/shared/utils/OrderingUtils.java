74ee0890b219611b9cc8e1ba007265d0841e3b30
bielxrd <gabriel.diasramos@hotmail.com>
2025-02-09 01:58:00 -0300

    feat: ordering utils para ordenacao de depositos

---
+++ src/main/java/br/com/picpay/shared/utils/OrderingUtils.java
@@ -0,0 +1,21 @@
+package br.com.picpay.shared.utils;
+
+import br.com.picpay.application.dtos.deposit_balance_history.DepositBalanceHistoryResponse;
+
+import java.util.List;
+
+public class OrderingUtils {
+    public static List<DepositBalanceHistoryResponse> applyOrdering(List<DepositBalanceHistoryResponse> depositBalanceHistoryResponseList,
+                                                                    boolean isDateOrdering,
+                                                                    boolean isValueOrdering) {
+        if (isDateOrdering) {
+            depositBalanceHistoryResponseList.sort((deposit1, deposit2) -> deposit1.getDepositDate().compareTo(deposit2.getDepositDate()));
+        }
+
+        if (isValueOrdering) {
+            depositBalanceHistoryResponseList.sort((deposit1, deposit2) -> Double.compare(deposit1.getValue(), deposit2.getValue()));
+        }
+
+        return depositBalanceHistoryResponseList;
+    }
+}
