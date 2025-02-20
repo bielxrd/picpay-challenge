e6a9cba5b7d4e11d2663ea6f1ffb0e071430935e
Gabriel Dias <gabriel.diasramos@hotmail.com>
2025-02-19 21:34:02 -0300

    feat: cache para transfers e invalidacao de cache

--- src/main/java/br/com/picpay/application/facade/TransferFacade.java
+++ src/main/java/br/com/picpay/application/facade/TransferFacade.java
@@ -9,21 +9,19 @@ import br.com.picpay.application.enums.TransferType;
 import br.com.picpay.application.services.balance.BalanceApplicationService;
 import br.com.picpay.application.services.transfer.TransferApplicationService;
 import br.com.picpay.application.services.wallet.WalletApplicationService;
+import br.com.picpay.infra.services.redis.RedisService;
 import br.com.picpay.infra.services.sqs.SqsService;
+import br.com.picpay.shared.utils.CacheUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
-import org.springframework.data.redis.core.*;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Log4j2
 @Service
@@ -33,7 +31,7 @@ public class TransferFacade {
     private final TransferApplicationService transferApplicationService;
     private final BalanceApplicationService balanceApplicationService;
     private final SqsService sqsService;
-    private final RedisTemplate redisTemplate;
+    private final RedisService redisService;
 
     @Value("${spring.cloud.aws.sqs.endpoint}")
     private String queueName;
@@ -42,26 +40,25 @@ public class TransferFacade {
     private String queueSmsName;
 
 
-    @Caching(evict = {
-            @CacheEvict(value = {"transfers-amount-received"},
-                    key = "#transferRequest.payerId"),
-            @CacheEvict(value = {"transfers-amount-received"},
-                    key = "#transferRequest.receiverId"),
-            @CacheEvict(value = {"user-profile"},
-            key = "#transferRequest.receiverId"),
-            @CacheEvict(value = {"user-profile"},
-                    key = "#transferRequest.payerId")
-    })
     public TransferResponse transfer(TransferRequest transferRequest) {
         TransferResponse transferResponse = executeTransfer(transferRequest);
         notifyEmailTransfer(transferResponse, transferRequest.getValue());
         notifySmsTransfer(transferResponse, transferRequest.getValue());
-        evictUserTransfersCache(transferResponse.getPayer().getId(), "transfers-payed");
+        List<Map.Entry<String, String>> cacheEntries = Arrays.asList(
+                Map.entry("transfers-amount-received", transferResponse.getPayer().getId().toString()),
+                Map.entry("transfers-amount-received", transferResponse.getReceiver().getId().toString()),
+                Map.entry("user-profile", transferResponse.getPayer().getId().toString()),
+                Map.entry("user-profile", transferResponse.getReceiver().getId().toString()),
+                Map.entry("transfers-received", transferResponse.getReceiver().getId().toString()),
+                Map.entry("transfers-payed", transferResponse.getPayer().getId().toString()),
+                Map.entry("transfers", transferResponse.getPayer().getId().toString()),
+                Map.entry("transfers", transferResponse.getReceiver().getId().toString())
+        );
+
+        evictCaches(cacheEntries);
         return transferResponse;
     }
 
-
-    @Transactional(rollbackFor = Exception.class)
     protected TransferResponse executeTransfer(TransferRequest transferRequest) {
         Map<UUID, WalletUserResponse> wallets = this.walletApplicationService.getWallets(transferRequest.getPayerId(), transferRequest.getReceiverId());
 
@@ -70,7 +67,8 @@ public class TransferFacade {
         return this.transferApplicationService.createTransfer(wallets.get(transferRequest.getPayerId()), wallets.get(transferRequest.getReceiverId()), transferRequest.getValue());
     }
 
-    private void notifyEmailTransfer(TransferResponse transferResponse, double value) {
+    @Async
+    protected void notifyEmailTransfer(TransferResponse transferResponse, double value) {
         try {
             Map<String, Object> messageAttributesReceiver = Map.of("receiver", transferResponse.getReceiver().getEmail(),
                     "value", value,
@@ -94,7 +92,8 @@ public class TransferFacade {
         }
     }
 
-    private void notifySmsTransfer(TransferResponse transferResponse, double value) {
+    @Async
+    protected void notifySmsTransfer(TransferResponse transferResponse, double value) {
         try {
             Map<String, Object> messageAttributesReceiver = Map.of(
                     "value", value,
@@ -117,41 +116,75 @@ public class TransferFacade {
         }
     }
 
-    public void evictUserTransfersCache(UUID userId, String cacheName) {
-        String keyPattern = cacheName + "::" + userId.toString() + "*";
-        Set<String> keys = redisTemplate.keys(keyPattern);
+    @Async
+    protected void evictCaches(List<Map.Entry<String, String>> entries) {
+        Set<String> cacheKeys = new HashSet<>();
 
-        if (keys != null && !keys.isEmpty()) {
-            redisTemplate.delete(keys);
-            log.info("Removed {} cache entries for user {} with pattern {}",
-                    keys.size(), userId, keyPattern);
-
-            if (log.isDebugEnabled()) {
-                keys.forEach(key -> log.debug("Removed cache key: {}", key));
+        entries.forEach(entry -> {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (key.contains("transfers") && !key.contains("amount")) {
+                cacheKeys.add(CacheUtils.buildKeyFilter(key, value));
+            } else {
+                cacheKeys.add(CacheUtils.buildKey(key, value));
             }
-        } else {
-            log.debug("No cache entries found for pattern: {}", keyPattern);
-        }
+        });
+
+        cacheKeys.forEach((key) -> {
+            if (key.contains("*")) {
+                log.info("Deleting keys by filter: {}", key);
+                redisService.deleteKeysByFilter(key);
+            } else {
+                log.info("Deleting key with no filter: {}", key);
+                redisService.delete(key);
+            }
+        });
+
     }
 
-
-    @Cacheable(value = "transfers-payed", key = "#userId + '-' + #pageNumber + '-' + #pageSize")
     public BaseResponsePageable getTransfersReceivedByUserIdAndPageable(UUID userId, int pageNumber, int pageSize) {
-        return this.transferApplicationService.getTransfersReceivedByUserId(userId, pageNumber, pageSize);
+        if (redisService.existsByKey(CacheUtils.buildKey("transfers-received", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize))))
+            return redisService.get(CacheUtils.buildKey("transfers-received", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)), BaseResponsePageable.class);
+
+        var transfersReceived = this.transferApplicationService.getTransfersReceivedByUserId(userId, pageNumber, pageSize);
+
+        redisService.save(CacheUtils.buildKey("transfers-received", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)), transfersReceived, Duration.ofMinutes(5));
+
+        return transfersReceived;
     }
 
-    @Cacheable(value = "transfers-payed", key = "#userId + '-' + #pageNumber + '-' + #pageSize")
     public BaseResponsePageable getTransfersPayedByUserIdAndPageable(UUID userId, int pageNumber, int pageSize) {
-        return this.transferApplicationService.getTransfersPayedByUserId(userId, pageNumber, pageSize);
+        if (redisService.existsByKey(CacheUtils.buildKey("transfers-payed", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)))) {
+            log.info("Cache key: {}", CacheUtils.buildKey("transfers-payed", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)));
+            return redisService.get(CacheUtils.buildKey("transfers-payed", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)), BaseResponsePageable.class);
+        }
+
+        var transfersPayed = this.transferApplicationService.getTransfersPayedByUserId(userId, pageNumber, pageSize);
+
+        redisService.save(CacheUtils.buildKey("transfers-payed", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)), transfersPayed, Duration.ofMinutes(5));
+
+        return transfersPayed;
     }
 
-    @Cacheable(value = "transfers-payed", key = "#userId + '-' + #pageNumber + '-' + #pageSize")
     public BaseResponsePageable getTransfersByUserIdAndPageable(UUID userId, int pageNumber, int pageSize) {
-        return this.transferApplicationService.getTransfersGenericByUserId(userId, pageNumber, pageSize);
+        if (redisService.existsByKey(CacheUtils.buildKey("transfers", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize))))
+            return redisService.get(CacheUtils.buildKey("transfers-payed", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)), BaseResponsePageable.class);
+
+        var transfers = this.transferApplicationService.getTransfersGenericByUserId(userId, pageNumber, pageSize);
+
+        redisService.save(CacheUtils.buildKey("transfers", userId.toString(), String.valueOf(pageNumber), String.valueOf(pageSize)), transfers, Duration.ofMinutes(5));
+
+        return transfers;
     }
 
-    @Cacheable(value = "transfers-received-amount", key = "#userId")
     public TransfersAmountListResponse getAmountTransferredFilteredByDateRange(UUID userId, LocalDateTime startDate, LocalDateTime endDate) {
-        return this.transferApplicationService.getTransfersAmount(userId, startDate, endDate);
+        if (redisService.existsByKey(CacheUtils.buildKey("transfers-amount-received", userId.toString())))
+            return redisService.get(CacheUtils.buildKey("transfers-amount-received", userId.toString()), TransfersAmountListResponse.class);
+
+        var transfers = this.transferApplicationService.getTransfersAmount(userId, startDate, endDate);
+
+        redisService.save(CacheUtils.buildKey("transfers-amount-received", userId.toString()), transfers, Duration.ofMinutes(5));
+
+        return transfers;
     }
 }
